ac1552d2387c99436032fb989516820415089737
ToBee17 <mechainadam@gmail.com>
2024-11-21 08:40:32 +0100

    install

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://ToBee17.github.io',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
